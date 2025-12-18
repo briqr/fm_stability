86605ac098cda3cf908cef04881c5f63e3681fae
Rania Briq <rania.briq@outlook.de>
2025-12-18 16:18:28 +0100

    add import statement

--- autoencoder.py
+++ autoencoder.py
@@ -1,4 +1,5 @@
 from timm import create_model
+import any_diffusion.autoencoders
 
 def get_autoencoder(vae_path, encoder_type="vq_gan_taming"):
     vae = create_model(model_name=encoder_type,  path=vae_path, pretrained=True)
